93a2cd16b8a2fba0c1c2dad25835e1051cc5dbca
Pablo Del Médico <pablo.delmedico@globant.com>
2016-02-01 14:54:16 -0300

    Creating services secion with masonry. Commenting Google maps

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
-(function ($) {
-    $.fn.scrollingTo = function( opts ) {
+(function($) {
+    $.fn.scrollingTo = function(opts) {
         var defaults = {
-            animationTime : 1000,
-            easing : '',
-            callbackBeforeTransition : function(){},
-            callbackAfterTransition : function(){}
+            animationTime: 1000,
+            easing: '',
+            callbackBeforeTransition: function() {},
+            callbackAfterTransition: function() {}
         };
 
-        var config = $.extend( {}, defaults, opts );
+        var config = $.extend({}, defaults, opts);
 
-        $(this).click(function(e){
+        $(this).click(function(e) {
             var eventVal = e;
             e.preventDefault();
 
-            var $section = $(document).find( $(this).data('section') );
-            if ( $section.length < 1 ) {
+            var $section = $(document).find($(this).data('section'));
+            if ($section.length < 1) {
                 return false;
             }
 
-            if ( $('html, body').is(':animated') ) {
-                $('html, body').stop( true, true );
+            if ($('html, body').is(':animated')) {
+                $('html, body').stop(true, true);
             }
 
             var scrollPos = $section.offset().top;
 
-            if ( $(window).scrollTop() == scrollPos ) {
+            if ($(window).scrollTop() == scrollPos) {
                 return false;
             }
 
             config.callbackBeforeTransition(eventVal, $section);
 
             $('html, body').animate({
-                'scrollTop' : (scrollPos+'px' )
-            }, config.animationTime, config.easing, function(){
+                'scrollTop': (scrollPos + 'px')
+            }, config.animationTime, config.easing, function() {
                 config.callbackAfterTransition(eventVal, $section);
             });
         });
@@ -41,30 +41,41 @@
 
 
 
-jQuery(document).ready(function(){
-	"use strict";
-	new WOW().init();
+jQuery(document).ready(function() {
+    "use strict";
+    new WOW().init();
 
 
-(function(){
- jQuery('.smooth-scroll').scrollingTo();
-}());
+    (function() {
+        jQuery('.smooth-scroll').scrollingTo();
+    }());
 
 });
 
 
 
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         if ($(window).scrollTop() > 50) {
-            $(".navbar-brand a").css("color","#fff");
+            $(".navbar-brand a").css("color", "#fff");
             $("#top-bar").removeClass("animated-header");
         } else {
-            $(".navbar-brand a").css("color","inherit");
+            $(".navbar-brand a").css("color", "inherit");
             $("#top-bar").addClass("animated-header");
         }
     });
 
+});
+
+/*Initializing Masonry gallery plugin*/
+$(document).ready(function() {
+    $('.grid').masonry({
+        // set itemSelector so .grid-sizer is not used in layout
+        itemSelector: '.grid-item',
+        // use element for option
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
 });
